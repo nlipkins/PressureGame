86fc66d4ae66bcbe5f111a9a50ea2a210efb3fcc
nlipkins <nathaniellipkins@gmail.com>
2020-03-06 10:49:58 -0600

    Confirmed rusty vehicle prefabs

--- Assets/Scripts/MoveRight.cs
+++ Assets/Scripts/MoveRight.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveRight : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,5 @@ public class MoveRight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
