3a0b0564a1c104e024a189ee849c31992a76f281
nlipkins <nathaniellipkins@gmail.com>
2020-03-19 16:02:43 -0400

    edited SpawnManager script
    - Edited spawn location of obstacles
    - Edited spawn times of obstacles
    + Still needs work

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,14 +8,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] carPrefab;
     public GameObject[] militaryPrefab;
 
+    //public Vector3 spawnPosition;
+    //public Vector3 spawnPosition2;
+    //public Vector3 spawnPosition3;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBarrels", 2, 2.5f);
+        InvokeRepeating("SpawnBarrels", 2, 2.25f);
 
-        InvokeRepeating("SpawnVehicles", 9.5f, 7f);
+        InvokeRepeating("SpawnVehicles", 9.5f, 9.3f);
 
-        InvokeRepeating("SpawnMilitary", 30, 10f);
+        InvokeRepeating("SpawnMilitary", 30, 8.25f);
     }
 
     // Update is called once per frame
@@ -27,24 +32,24 @@ public class SpawnManager : MonoBehaviour
     void SpawnBarrels()
     {
         int obstacleIndex = Random.Range(0, barrelPrefab.Length);
-        Vector3 spawnPos = new Vector3(30, 0, 0);
+        Vector3 spawnPosition = new Vector3(30, 0, -1.5f);
 
-        Instantiate(barrelPrefab[obstacleIndex], spawnPos, barrelPrefab[obstacleIndex].transform.rotation);
+        Instantiate(barrelPrefab[obstacleIndex], spawnPosition, barrelPrefab[obstacleIndex].transform.rotation);
     }
 
     void SpawnVehicles()
     {
         int carIndex = Random.Range(0, carPrefab.Length);
-        Vector3 spawnPos2 = new Vector3(30, 0, 0);
+        Vector3 spawnPosition2 = new Vector3(30, 0, -1.5f);
 
-        Instantiate(carPrefab[carIndex], spawnPos2, carPrefab[carIndex].transform.rotation);
+        Instantiate(carPrefab[carIndex], spawnPosition2, carPrefab[carIndex].transform.rotation);
     }
 
     void SpawnMilitary()
     {
         int militaryIndex = Random.Range(0, militaryPrefab.Length);
-        Vector3 spawnPos3 = new Vector3(30, 0, 0);
+        Vector3 spawnPosition3 = new Vector3(30, 0, -1.5f);
 
-        Instantiate(militaryPrefab[militaryIndex], spawnPos3, militaryPrefab[militaryIndex].transform.rotation);
+        Instantiate(militaryPrefab[militaryIndex], spawnPosition3, militaryPrefab[militaryIndex].transform.rotation);
     }
 }
