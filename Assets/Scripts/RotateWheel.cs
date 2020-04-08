c6b730ea99ea0059ac232b7b012698583286a6cf
nlipkins <nathaniellipkins@gmail.com>
2020-04-08 14:33:01 -0400

    Edited RotateWheel script
    - Wheels now stop turning once game is over

--- Assets/Scripts/RotateWheel.cs
+++ Assets/Scripts/RotateWheel.cs
@@ -6,15 +6,21 @@ public class RotateWheel : MonoBehaviour
 {
     private float wheelSpeed = 20;
 
+    private PlayerController pcs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pcs = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(wheelSpeed, 0f, 0f));
+        if(pcs.gameOver == false)
+        {
+            transform.Rotate(new Vector3(wheelSpeed, 0f, 0f));
+        }
+        //transform.Rotate(new Vector3(wheelSpeed, 0f, 0f));
     }
 }
