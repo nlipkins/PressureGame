74d22a218f3268e84b52cf8236d92aaf29ee7af7
nlipkins <nathaniellipkins@gmail.com>
2020-04-08 15:51:49 -0400

    Confirmed the repeating street in Repeat script
    - Gives the player an illusion that the subjects are advancing

--- Assets/Scripts/Repeat.cs
+++ Assets/Scripts/Repeat.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Repeat : MonoBehaviour
 {
+    private Vector3 startPos;
+
+    private float repeatWidth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+
+        repeatWidth = GetComponent<BoxCollider>().size.x / 1.53f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
     }
 }
