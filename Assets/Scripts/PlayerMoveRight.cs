c32b609078e40f131618f4d0a3c435758a2172b9
nlipkins <nathaniellipkins@gmail.com>
2020-05-15 16:09:47 -0400

    Edited Tutorial Level
    - Created PlayerMoveRight script
    - Edited position of Player and Enemy
    - Remade the Player Setup prefab
    - Started to add a Trigger for next menu

---
+++ Assets/Scripts/PlayerMoveRight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMoveRight : MonoBehaviour
+{
+
+    public float speed = 10;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject.CompareTag("Next Level"))
+        {
+
+        }
+
+    }
+}
